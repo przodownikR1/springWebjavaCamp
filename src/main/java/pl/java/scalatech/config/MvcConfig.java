8a7c926fd555c23ed234f14d4484362cf5ea3c09
Slawomir Borowiec <przodownik@tlen.pl>
2015-11-20 15:06:17 +0100

    negotiation start

--- src/main/java/pl/java/scalatech/config/MvcConfig.java
+++ src/main/java/pl/java/scalatech/config/MvcConfig.java
@@ -1,49 +1,116 @@
 package pl.java.scalatech.config;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Maps.newHashMap;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.MediaType;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 
 import pl.java.scalatech.converters.AmountFormatAnnotationFormatterFactory;
 import pl.java.scalatech.converters.AmountFormatter;
 import pl.java.scalatech.converters.LongToUserConverter;
 import pl.java.scalatech.converters.StringToUserConverter;
+import pl.java.scalatech.domain.User;
+
 @Configuration
 @ComponentScan({ "pl.java.scalatech.controller", "pl.java.scalatech.initializer" })
 @EnableWebMvc
-public class MvcConfig extends WebMvcConfigurerAdapter{
+@Import(ThymeleafConfig.class)
+public class MvcConfig extends WebMvcConfigurerAdapter {
 
-@Autowired
-private StringToUserConverter stringToUserConverter;
-@Autowired
-private LongToUserConverter longToUserConverter;
-@Autowired
-private AmountFormatter amountFormatter;
-@Autowired
-private AmountFormatAnnotationFormatterFactory amountFormatAnnotationFormatterFactory;
+    @Autowired
+    private StringToUserConverter stringToUserConverter;
+    @Autowired
+    private LongToUserConverter longToUserConverter;
+    @Autowired
+    private AmountFormatter amountFormatter;
+    @Autowired
+    private AmountFormatAnnotationFormatterFactory amountFormatAnnotationFormatterFactory;
 
-@Override
-public void addFormatters(FormatterRegistry registry) {
-    super.addFormatters(registry);
-    registry.addConverter(stringToUserConverter);
-    registry.addConverter(longToUserConverter);
-    registry.addFormatter(amountFormatter);
-    registry.addFormatterForFieldAnnotation(amountFormatAnnotationFormatterFactory);
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        super.addFormatters(registry);
+        registry.addConverter(stringToUserConverter);
+        registry.addConverter(longToUserConverter);
+        registry.addFormatter(amountFormatter);
+        registry.addFormatterForFieldAnnotation(amountFormatAnnotationFormatterFactory);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("hello");
+
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        super.configureContentNegotiation(configurer);
+        Map<String, MediaType> mediaTypeMap = newHashMap();
+        mediaTypeMap.put("atom", MediaType.APPLICATION_ATOM_XML);
+        mediaTypeMap.put("html", MediaType.TEXT_HTML);
+        mediaTypeMap.put("json", MediaType.APPLICATION_JSON);
+        mediaTypeMap.put("xml", MediaType.APPLICATION_XML);
+        
+        configurer.favorPathExtension(true).favorParameter(true).parameterName("mediaType").ignoreAcceptHeader(true).useJaf(false)
+                .defaultContentType(MediaType.APPLICATION_JSON).mediaTypes(mediaTypeMap);
+                
+                
+    }
+
+
+
+    @Bean
+    public ViewResolver viewResolver(ContentNegotiationManager manager, View jsonView, View xmlView, ThymeleafViewResolver thymeleafResolver) {
+        ContentNegotiatingViewResolver viewResolver = new ContentNegotiatingViewResolver();
+        viewResolver.setContentNegotiationManager(manager);
+        viewResolver.setOrder(1);
+        
+        List<ViewResolver> viewResolverList = newArrayList();
+        viewResolverList.add(thymeleafResolver);
+        viewResolver.setOrder(1);
+        viewResolver.setViewResolvers(viewResolverList);
+        viewResolver.setDefaultViews(Arrays.asList(jsonView, xmlView));
+        return viewResolver;
+    }
+
+
+@Bean
+public View jsonView() {
+    return new MappingJackson2JsonView();
 }
 
-@Override
-public void addViewControllers(ViewControllerRegistry registry) {
-       registry.addViewController("/").setViewName("hello");
+@Bean
+public Jaxb2Marshaller jaxb2Marshaller() {
+    Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+    jaxb2Marshaller.setClassesToBeBound(User.class);
+    return jaxb2Marshaller;
+}
+
 
 }
 
-/*@Override
-public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-    super.
-}*/
-
-}
+  /*@Override
+  public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+  super.
+  }*/
+ 
--- src/main/java/pl/java/scalatech/domain/User.java
+++ src/main/java/pl/java/scalatech/domain/User.java
@@ -1,5 +1,7 @@
 package pl.java.scalatech.domain;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,7 @@ import pl.java.scalatech.annotations.AmountFormat;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@XmlRootElement
 public class User {
 
     private Long id;
