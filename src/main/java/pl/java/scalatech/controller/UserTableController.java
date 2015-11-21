27cd7d8a1dcc57274b0f3044b590ebdce8faa3d7
Slawomir Borowiec <przodownik@tlen.pl>
2015-11-21 22:20:41 +0100

    redirect test

--- src/main/java/pl/java/scalatech/controller/UserTableController.java
+++ src/main/java/pl/java/scalatech/controller/UserTableController.java
@@ -6,13 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.extern.slf4j.Slf4j;
 import pl.java.scalatech.domain.User;
 import pl.java.scalatech.repository.UserRepository;
 
 @Controller
+@Slf4j
 public class UserTableController {
     @Autowired
     private UserRepository userRepository;
@@ -36,6 +40,19 @@ public class UserTableController {
             return "user";
         }
         return "redirect:/results";
+    }
+
+    @RequestMapping(value ="/redirectUser/{str}/str", method=RequestMethod.GET)
+    @ResponseBody String redirectStringUser(@PathVariable String str,Model model){
+           log.info("+ +++ model : {}",model);
+           return "redirect : to : "+str ;
+    }
+
+    @RequestMapping(value ="/redirectUser/{id}", method=RequestMethod.GET)
+    String redirectUser(@PathVariable Long id,Model model){
+        User loaded = userRepository.findById(id);
+        model.addAttribute("username", loaded);
+        return "redirect:/redirectUser/"+loaded.getLogin()+"/str";
     }
 
 }
