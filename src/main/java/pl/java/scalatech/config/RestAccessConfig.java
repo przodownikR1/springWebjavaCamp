8e6954876728bee060e980222cedf18394a0516d
Slawomir Borowiec <przodownik@tlen.pl>
2015-11-21 13:58:22 +0100

    restTemplateConfig

---
+++ src/main/java/pl/java/scalatech/config/RestAccessConfig.java
@@ -0,0 +1,38 @@
+package pl.java.scalatech.config;
+
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+public class RestAccessConfig {
+
+    @Bean(name = "restTemplate")
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setErrorHandler(new MyRestClientErrorHandler());
+        return restTemplate;
+    }
+
+}
+
+@Slf4j
+class MyRestClientErrorHandler extends DefaultResponseErrorHandler {
+
+
+    @Override
+    public void handleError(ClientHttpResponse response) throws IOException {
+        String body = IOUtils.toString(response.getBody());
+       log.info("+++ status code : {} , response :  {}",response.getStatusCode(),body);
+       super.handleError(response);
+
+    }
+
+}
